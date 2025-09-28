5b3ee0b8cb4a18f13a4adec33e7f3c3e0399bd88
mohamed <mohamedissaoui2468@gmail.com>
2025-09-28 21:26:37 +0100

    solved day 1

---
+++ 1.py
@@ -0,0 +1,61 @@
+# Advent of Code 2024 - Day 1
+# Work done by Mohamed Issaoui for the application of wandercraft intership 
+
+
+
+def solve_problem_part1(): 
+    leftlist = []
+    rightlist = []
+    with open("input.txt", "r") as file:
+        for line in file:
+            left, right = line.strip().split()
+            leftlist.append(int(left))
+            rightlist.append(int(right))
+    #sort the lists
+    leftlist.sort()
+    rightlist.sort()
+
+    #calculate the distance 
+    total_distance = 0
+    for i in range(len(leftlist)):
+        distance = abs(int(leftlist[i]) - int(rightlist[i]))
+        total_distance += distance
+
+    #print(total_distance)  
+    return total_distance   
+
+def solve_problem_part2():
+    
+    leftlist = []
+    rightlist = []
+    with open("input.txt", "r") as file:
+        for line in file:
+            left, right = line.strip().split()
+            leftlist.append(int(left))
+            rightlist.append(int(right))
+
+        #calculate similarity score
+        similarity_score = 0
+        sim=0
+        for i in range(len(leftlist)):
+            for j in range(len(rightlist)):
+                if leftlist[i] == rightlist[j]:
+                    sim+=1
+                similarity_score += leftlist[i]*sim
+                sim=0
+                
+ 
+    return similarity_score
+
+    
+def main(): 
+    try:
+        #result = solve_problem_part1()
+        result = solve_problem_part2()
+        print(f"The result is: {result}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
